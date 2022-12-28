47835cc92108d7b7d81d7ee6cc19bc38daacb9bd
SajalRuhela017 <ruhela.sajal017@gmail.com>
2022-12-28 22:47:06 +0530

    Pushed

---
+++ ifElseStatements.py
@@ -0,0 +1,10 @@
+#VOTING ELIGIBLITY CHECK
+
+age = input("Enter your age: ")
+if(int(age) >= 18):
+    print("You are an adult")
+    print("You can cast your vote")
+else:
+    print("You are not an adult")
+    print("You cannot cast your vote")
+print("THANK YOU!")
