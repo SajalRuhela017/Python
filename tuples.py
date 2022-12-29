4f6587b68ca5075f54e17ad1523a54da641eb4f1
SajalRuhela017 <ruhela.sajal017@gmail.com>
2022-12-29 14:13:03 +0530

    Pushed

--- tuples.py
+++ tuples.py
@@ -1,5 +1,8 @@
-marks = (81, 94, 97)
+marks = (81, 94, 97, 98, 97)
+
+# marks[0] = 99                   #will throw error as we cannot change the values in tuple
 
 #.count and .index
+print(marks.index(94))
 
-marks[0] = 99                   #will throw error as we cannot change the values in tuple
+print(marks.count(97))
