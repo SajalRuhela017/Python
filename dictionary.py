d6f98a65e7ac9610a0f7164ba91ad8a8a308157e
SajalRuhela017 <ruhela.sajal017@gmail.com>
2022-12-30 21:53:00 +0530

    First Commit

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+#Dictionary holding student and his father name
+
+students = {"Pappu" : "Rajiv" , "Kunal" : "Ramprakash"}
+
+print(students)
+
+students["Ram"] = "Dashrath"            #added one more entry
+
+print(students)
+
+students["Pappu"] = "Modi"              #modified one entry
+
+print(students)
---
+++ set.py
@@ -0,0 +1,5 @@
+distance = {94, 12, 87, 97, 85, 87}
+print(distance[0])           #We cannot access the set with index.... This will give error
+
+for gap in distance:
+    print(gap)
